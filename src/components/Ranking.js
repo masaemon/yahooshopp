35e68eb44cd6102a4b4a8f054253e3be697c3d12
masashi orui <masaemonmasashi@gmail.com>
2018-11-20 01:07:13 +0900

    implement ui

---
+++ src/components/Ranking.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import Card, { CardMedia, CardActions, CardContent } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import PropTypes from 'prop-types'
+
+export default class Ranking extends React.Component {
+  componentWillMount() {
+    this.props.onMount(this.props.categoryId)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.categoryId !== nextProps.categoryId) {
+      this.props.onUpdate(nextProps.categoryId)
+    }
+  }
+
+  render() {
+    const { category, ranking, error } = this.props
+    return (
+      <div>
+        <h2>{
+          typeof category !== 'undefined'
+            ? `${category.name}のランキング`
+            : ''
+        }</h2>
+
+        {(() => {
+          if (error) {
+            return <p>エラーが発生しました。リロードしてください。</p>
+          } else if (typeof ranking === 'undefined') {
+            return <p> 読み込み中...</p>
+          } else {
+            return ranking.map((item, i) => (
+              <Card
+                key={`ranking-item-${item.code}`}
+                style={{ maxWidth: '500px', margin: '32px auto' }}
+              >
+                <CardMedia
+                  image={item.imageUrl}
+                  title={`${i + 1}位 ${item.name}`}
+                  style={{ height: '200px' }}
+                />
+                <CardContent>
+                  <Typography type="title">
+                    {`${i + 1} 位 ${item.name}`}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    raised
+                    color="secondary"
+                    fullwidth
+                    href={item.url}
+                  >商品ページへ</Button>
+                </CardActions>
+              </Card>
+            ))
+          }
+        })()}
+      </div>
+    )
+  }
+}
+
+Ranking.propTypes = {
+  categoryId: PropTypes.string,
+  onMount: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  category: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  }),
+  ranking: PropTypes.arrayOf(
+    PropTypes.shape({
+      code: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired
+    })
+  ),
+  error: PropTypes.bool.isRequired
+}
+
+Ranking.defaultProps = {
+  categoryId: '1'
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import createBrowserHistory from 'history/createBrowserHistory'
+import App from './App'
+import createStore from './createStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(history)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
